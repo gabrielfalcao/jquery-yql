5494cda7fbfd12d5504ad7e838f28be29f60734e
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-04-30 16:34:06 -0300

    Added optional error callback to offer error handling to plugin users.

--- tests/test.yql.js
+++ tests/test.yql.js
@@ -155,3 +155,24 @@ asyncTest('It can fetch location data by IP address', function() {
               });
 
           });
+
+test('It should call the optional error callback when ajax fails', function() {
+    expect(1);
+    
+    $.ajax = function (params) {
+        params.error("error data");
+    }
+
+    $.yql("SELECT * FROM unknown_table WHERE param=#{param} ", {param: "blah"},
+        function (data) {
+            ok(false, "should not reach here");
+            start();
+        },
+        function (error_data) {
+            equal(error_data, "error data");
+            start();
+        }
+    );
+    
+    $.ajax = oldAjax;
+});
